d1cebf8fa2a725efe312861707c5ffc1d1f535cf
liming <liming@example.com>
2026-03-06 14:48:24 +0800

    reorganize project structure

--- server/dist/ai/ai.service.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class AiService {
-    private openai;
-    constructor();
-    diagnose(vehicleInfo: any, symptom: string): Promise<{
-        diagnosis: string;
-        timestamp: string;
-    }>;
-}
